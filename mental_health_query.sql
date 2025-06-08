0a009681bc18f0e2c9ea2afdddc49a7b24f81bfe
Chisom Fortune Chinedu-Onuoha <110789689+chisomudeogu@users.noreply.github.com>
2025-06-08 16:29:41 +0100

    Add files via upload

---
+++ mental_health_query.sql
@@ -0,0 +1,12 @@
+
+-- SQL Query: Average Mental Health Scores by Length of Stay
+
+SELECT stay,
+	COUNT(*) AS count_int, 
+	ROUND(AVG(todep), 2) AS average_phq, 
+	ROUND(AVG(tosc), 2) AS average_scs,
+	ROUND(AVG(toas), 2) AS average_as
+FROM students
+WHERE inter_dom = 'Inter'
+GROUP BY stay
+ORDER BY stay DESC;
